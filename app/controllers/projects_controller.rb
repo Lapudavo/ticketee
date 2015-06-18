448ef29369a5ea2e1ceaab57f7355c10c5aa4c40
Lapudavo <gamerekx@gmail.com>
2015-06-18 23:01:33 +0200

    Implement delete functionality for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -23,6 +23,30 @@ class ProjectsController < ApplicationController
         @project = Project.find(params[:id])
     end
 
+    def edit
+        @project = Project.find(params[:id])
+    end
+
+    def update
+        @project = Project.find(params[:id])
+        if @project.update(project_params)
+            flash[:notice] = "Project has been updated."
+            redirect_to @project
+        else
+            flash[:alert] = "Project has not been updated."
+            render "edit"
+        end
+    end
+
+    def destroy
+        @project = Project.find(params[:id])        
+        @project.destroy
+        
+        flash[:notice] = "Project has been destroyed."
+        
+        redirect_to projects_path
+    end
+
     private
         def project_params
             params.require(:project).permit(:name, :description)
