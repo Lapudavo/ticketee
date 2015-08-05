6167d7a1d5de10fb6009e3e1ab1bf0677319e500
Lapudavo <gamerekx@gmail.com>
2015-08-05 19:32:36 +0200

    Provide an 'Add another file' link that uses Javascript so that users can upload more than one file

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -57,22 +57,24 @@ feature "Creating Tickets" do
 #       end
 #   end
 
-    scenario "Creating a ticket with an attachment" do
+    scenario "Creating a ticket with an attachment", js: true do
         fill_in "Title", with: "Add documentation for blink tag"
         fill_in "Description", with: "The blink tag has a speed attribute"
 
         attach_file "File #1", Rails.root.join("spec/fixtures/speed.txt")
+
+        click_link "Add another file"
         attach_file "File #2", Rails.root.join("spec/fixtures/spin.txt")
-        attach_file "File #3", Rails.root.join("spec/fixtures/gradient.txt")
 
         click_button "Create Ticket"
+#       attach_file "File #3", Rails.root.join("spec/fixtures/gradient.txt")
 
         expect(page).to have_content("Ticket has been created.")
 
         within("#ticket .assets") do
             expect(page).to have_content("speed.txt")
             expect(page).to have_content("spin.txt")
-            expect(page).to have_content("gradient.txt")
+#           expect(page).to have_content("gradient.txt")
         end
     end
 end
